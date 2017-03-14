e692dcf912704cbee7b1d943cc3ba54c88670828
Benjamin <benjamin.kinnard1@gmail.com>
2017-03-14 14:25:29 +0000

    finished part A

--- specs/person_spec.rb
+++ specs/person_spec.rb
@@ -8,6 +8,11 @@ class TestPerson < MiniTest::Test
     @person1 = Person.new("Ben", 27)
   end
 
-  
+  def test_person_name
+    assert_equal("Ben", @person1.name)
+  end
 
+  def test_person_age
+    assert_equal(27, @person1.age)
+  end 
 end
